e02633cb15465668344f86b26b6c5da30b63870c
Alex <alexdesouzasilva94@gmail.com>
2024-10-12 06:25:16 -0300

    args

--- publish.sh
+++ publish.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+file_path=$1
+
 echo "File Path: $file_path"
 
+mvn=$(which mvn)
 
-# mvn deploy -B -f pom.xml -s ./settings.xml \
-#                     -Durl=${{ vars.NEXUS_URL }} \
-#                     -Dfile=target/$ARTIFACTID.jar \
-#                     -DgroupId=$GROUPID \
-#                     -DartifactId=$ARTIFACTID \
-#                     -Dpackaging=jar \
-#                     -Dversion=${{ steps.variables.outputs.version }} \
-#                     -DrepositoryId=${{ vars.NEXUS_REPO_NAME_SNAP }}
+# $mvn deploy -B -f pom.xml -s ./settings.xml \
+#     -Dfile=target/$ARTIFACTID.jar \
+#     -DgroupId=$GROUPID \
+#     -DartifactId=$ARTIFACTID \
+#     -Dpackaging=jar \
+#     -Dversion=${{ steps.variables.outputs.version }} \
+#     -DrepositoryId=${{ vars.NEXUS_REPO_NAME_SNAP }} \
+#     -Durl=${{ vars.NEXUS_URL }} \
