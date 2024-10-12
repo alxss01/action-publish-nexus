9cd37b693761cda8859fcb16800cc81c3089a8ed
Alex <alexdesouzasilva94@gmail.com>
2024-10-12 06:17:35 -0300

    publish nexus

--- publish.sh
+++ publish.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "File Path: $file_path"
+
+
+# mvn deploy -B -f pom.xml -s ./settings.xml \
+#                     -Durl=${{ vars.NEXUS_URL }} \
+#                     -Dfile=target/$ARTIFACTID.jar \
+#                     -DgroupId=$GROUPID \
+#                     -DartifactId=$ARTIFACTID \
+#                     -Dpackaging=jar \
+#                     -Dversion=${{ steps.variables.outputs.version }} \
+#                     -DrepositoryId=${{ vars.NEXUS_REPO_NAME_SNAP }}
